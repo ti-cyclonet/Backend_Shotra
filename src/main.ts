b73ab641fdc50b79c1184ea6ffd924cfa656ff31
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2026-09-02 12:12:30 -0500

    chore(deploy): trigger redeploy con fix del engine de Prisma

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+// Deploy: engine de Prisma sobre node:20-slim (debian-openssl-3.0.x)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
